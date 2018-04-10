529b169ac7ecfd541c1a0ceecc808836f6725e2d
WD1Master <akmksi211@gmail.com>
2018-04-09 22:26:50 -0400

    Solvency BTC

--- lib/daemons/amqp_daemon.rb
+++ lib/daemons/amqp_daemon.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
 # You might want to change this
-ENV["RAILS_ENV"] ||= "development"
+#ENV["RAILS_ENV"] ||= "development"
 
-root = File.expand_path(File.dirname(__FILE__))
+root = File.expaSnd_path(File.dirname(__FILE__))
 root = File.dirname(root) until File.exists?(File.join(root, 'config'))
 Dir.chdir(root)
 
